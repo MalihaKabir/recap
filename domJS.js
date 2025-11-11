9c46820fa4bedb9a44e6046a300327780b1895bf
Maliha Abdul Kabir <malihaabdulkabir@gmail.com>
2025-11-11 20:22:59 +0600

    practiced eventListener and callback function

--- domJS.js
+++ domJS.js
@@ -33,3 +33,74 @@ console.log(document.querySelector("li").getAttribute("random"));
 
 
 document.querySelector("h2").style.background = "yellow";
+
+// let button = document.getElementsByTagName("button")[0]
+
+// button.addEventListener("click", function () { 
+//     console.log("Click Me!!!");
+    
+// })
+
+// "addEventListener" accepts the event and what we want to do when the event happens - a function.
+
+
+
+
+// ----------->> Practice of eventListener and Callback function <<--------------
+let userInput = document.getElementById("userInput");
+let button = document.getElementById("enterBtn");
+let ulChildAdd = document.querySelector("ul")
+
+
+// button.addEventListener("click", function () {
+//     if (userInput.value.length > 0) {
+//         let liAdd = document.createElement("li")
+//         liAdd.appendChild(document.createTextNode(userInput.value)) 
+//         ulChildAdd.appendChild(liAdd)
+//         userInput.value = "";
+//     }
+    
+// })
+
+// userInput.addEventListener("keydown", function (event) {
+//     if ((userInput.value.length > 0) && (event.key === "Enter")) {
+//         let liAdd = document.createElement("li")
+//         liAdd.appendChild(document.createTextNode(userInput.value)) 
+//         ulChildAdd.appendChild(liAdd)
+//         userInput.value = "";
+//     }
+// })
+
+
+// ------>> OPTIMIZE THE ABOVE eventListener/s USING "DRY = WRITE ONCE, USE MANY TIMES" <<-------
+function userInputLength() {
+    return userInput.value.length; 
+    // Every function does something when you call it.
+    // But only some functions need to tell you something back.
+    // Something "they need to return after" doing some action.
+}
+
+function addItem() {
+    let liAdd = document.createElement("li")
+    liAdd.appendChild(document.createTextNode(userInput.value)) 
+    ulChildAdd.appendChild(liAdd)
+    userInput.value = "";
+}
+
+function addListAfterMouseClick() {
+    if (userInputLength() > 0) {
+        addItem()
+    }
+}
+
+function addListAfterPressEnter(event) {
+    if ((userInputLength() > 0) && (event.key === "Enter")) {
+        addItem()
+    }
+}
+
+button.addEventListener("click", addListAfterMouseClick)
+
+userInput.addEventListener("keydown", addListAfterPressEnter)
+
+
